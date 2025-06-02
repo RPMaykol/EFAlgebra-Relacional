44c26364a867e9a68ba364505adbccca43d7473a
MRMaykol <mroccap@ucvvirtual.edu.pe>
2025-06-01 23:17:07 -0500

    Agrega proyecto

---
+++ ADONetEF/OperacionesUnarias.aspx
@@ -0,0 +1,33 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="OperacionesUnarias.aspx.cs" Inherits="ADONetEF.OperacionesUnarias" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div>
+            <p>Operaciones Unarias - Seleccion</p>
+            <p>
+                <asp:Button Text="Consulta 1" runat="server" ID="btnConsulta1" OnClick="btnConsulta1_Click" />
+                <asp:Button Text="Consulta 2" runat="server" ID="btnConsulta2" OnClick="btnConsulta2_Click"/>
+                <asp:Button Text="Consulta 3" runat="server" ID="btnConsulta3" OnClick="btnConsulta3_Click"/>
+                <asp:Button Text="Consulta 4" runat="server" ID="btnConsulta4" OnClick="btnConsulta4_Click" />
+                <asp:Button Text="Consulta 5" runat="server" ID="btnConsulta5" OnClick="btnConsulta5_Click"/>
+                <asp:Button Text="Consulta 6" runat="server" ID="btnConsulta6" OnClick="btnConsulta6_Click"/>
+                <asp:Button Text="Consulta 7" runat="server" ID="btnConsulta7" OnClick="btnConsulta7_Click"/>
+            </p>
+            <p>
+                <asp:Button Text="Consulta 8" runat="server" ID="btnConsulta8" OnClick="btnConsulta8_Click"/>
+                <asp:Button Text="Consulta 9" runat="server" ID="btnConsulta9" OnClick="btnConsulta9_Click"/>
+            </p>
+            <p>
+                <asp:GridView runat="server" ID="gvConsulta"></asp:GridView>
+            </p>
+        </div>
+    </form>
+</body>
+</html>
---
+++ ADONetEF/OperacionesUnarias.aspx.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace ADONetEF
+{
+    public partial class OperacionesUnarias : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        //Declarar el objeto para el mapeado objeto relacional
+        private NorthwindEntities nor = new NorthwindEntities();
+        protected void gvConsulta_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void btnConsulta1_Click(object sender, EventArgs e)
+        {
+            var consulta = from C in nor.Products
+                           select C;
+            gvConsulta.DataSource = consulta.ToList();
+            gvConsulta.DataBind();
+        }
+
+        protected void btnConsulta2_Click(object sender, EventArgs e)
+        {
+            var consulta = from C in nor.Products
+                           where C.UnitPrice >= 50
+                           select C;
+            gvConsulta.DataSource = consulta.ToList();
+            gvConsulta.DataBind();
+        }
+
+        protected void btnConsulta3_Click(object sender, EventArgs e)
+        {
+            var consulta = from p in nor.Products
+                           where p.QuantityPerUnit.Contains("bottles") && p.UnitsInStock > 20
+                           select p;
+
+            gvConsulta.DataSource = consulta.ToList();
+            gvConsulta.DataBind();
+        }
+
+        protected void btnConsulta4_Click(object sender, EventArgs e)
+        {
+            var consulta = from P in nor.Orders
+                           select new
+                           {
+                               IDPedido = P.OrderID, 
+                               FechaOrden = P.OrderDate, 
+                               CodigoCliente = P.CustomerID 
+                           };
+            gvConsulta.DataSource = consulta.ToList();
+            gvConsulta.DataBind();
+        }
+
+        protected void btnConsulta5_Click(object sender, EventArgs e)
+        {
+            var consulta = from Prod in nor.Products
+                           select new
+                           {
+                               CodigoProducto = Prod.ProductID, 
+                               NombreProducto = Prod.ProductName, 
+                               PrecioUnitario = Prod.UnitPrice 
+                           };
+            gvConsulta.DataSource = consulta.ToList();
+            gvConsulta.DataBind();
+        }
+
+        protected void btnConsulta6_Click(object sender, EventArgs e)
+        {
+            var consulta = from E in nor.Employees
+                           select new
+                           {
+                               IDEmpleado = E.EmployeeID, 
+                               NombreCompleto = E.FirstName + " " + E.LastName, 
+                               Cargo = E.Title 
+                           };
+            gvConsulta.DataSource = consulta.ToList();
+            gvConsulta.DataBind();
+        }
+
+        protected void btnConsulta7_Click(object sender, EventArgs e)
+        {
+            var consulta = from Prod in nor.Products
+                           select new
+                           {
+                               NuevoNombreProducto = Prod.ProductName, 
+                               PrecioUnitario = Prod.UnitPrice 
+                           };
+            gvConsulta.DataSource = consulta.ToList();
+            gvConsulta.DataBind();
+        }
+
+        protected void btnConsulta8_Click(object sender, EventArgs e)
+        {
+            var consulta = from P in nor.Orders
+                           select new
+                           {
+                               FechaOrden = P.OrderDate, 
+                               CodigoCliente = P.CustomerID 
+                           };
+            gvConsulta.DataSource = consulta.ToList();
+            gvConsulta.DataBind();
+        }
+
+        protected void btnConsulta9_Click(object sender, EventArgs e)
+        {
+            var consulta = from E in nor.Employees
+                           select new
+                           {
+                               IDEmpleado = E.EmployeeID, 
+                               NombreCompleto = E.FirstName + " " + E.LastName 
+                           };
+            gvConsulta.DataSource = consulta.ToList();
+            gvConsulta.DataBind();
+        }
+    }
+
+}
